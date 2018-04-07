f87a62c2edbb6979250cf2e747f88b623fc0c907
jffernan <jffernan@comcast.net>
2018-04-07 15:05:30 -0700

    add user_id column to reviews db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180401004635) do
+ActiveRecord::Schema.define(version: 20180407220127) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
